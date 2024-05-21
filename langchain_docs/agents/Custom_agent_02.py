b2e94704bea0573f826313c1122fc75722d9f225
Sean <x90417@gmail.com>
2024-05-21 09:44:06 +0800

    update

--- langchain_docs/agents/Custom_agent_02.py
+++ langchain_docs/agents/Custom_agent_02.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
-import os
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# import os
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 #################################################
 ### Load the LLM
