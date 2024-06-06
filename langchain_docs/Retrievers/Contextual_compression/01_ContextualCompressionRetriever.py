2c73467fa01ad9c20dd5c195e499d02df7221ca6
Sean <x90417@gmail.com>
2024-06-06 21:59:48 +0800

    updated

---
+++ langchain_docs/Retrievers/Contextual_compression/01_ContextualCompressionRetriever.py
@@ -0,0 +1,31 @@
+def pretty_print_docs(docs):
+    print(
+        f"\n{'-' * 100}\n".join(
+            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
+        )
+    )
+
+from langchain_community.document_loaders.text import TextLoader
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain_openai import OpenAI
+
+documents = TextLoader("../../../text_files/state_of_the_union.txt").load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+retriever = FAISS.from_documents(texts, OpenAIEmbeddings()).as_retriever()
+
+
+llm = OpenAI(temperature=0)
+compressor = LLMChainExtractor.from_llm(llm)
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor, base_retriever=retriever
+)
+
+compressed_docs = compression_retriever.invoke(
+    "What did the president say about Ketanji Jackson Brown"
+)
+pretty_print_docs(compressed_docs)
---
+++ langchain_docs/Retrievers/Contextual_compression/02_EmbeddingsFilter.py
@@ -0,0 +1,39 @@
+def pretty_print_docs(docs):
+    print(
+        f"\n{'-' * 100}\n".join(
+            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
+        )
+    )
+
+from langchain_community.document_loaders.text import TextLoader
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain_openai import OpenAI
+
+documents = TextLoader("../../../text_files/state_of_the_union.txt").load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+retriever = FAISS.from_documents(texts, OpenAIEmbeddings()).as_retriever()
+
+
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings()
+embeddings_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.76)
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=embeddings_filter, base_retriever=retriever
+)
+
+import time
+start_time = time.perf_counter()
+compressed_docs = compression_retriever.invoke(
+    "What did the president say about Ketanji Jackson Brown"
+)
+print(f"Time taken: {time.perf_counter() - start_time}")
+for doc in compressed_docs:
+    print(len(doc.page_content))
+pretty_print_docs(compressed_docs)
