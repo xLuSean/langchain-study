4510e5eb0c215fa7afa01a7aaf0ce86dcf7c2dee
sean(TM Mac) <x90417@gmail.com>
2024-12-12 17:33:53 +0800

    update

--- langchain_docs/vector_store/faiss/save_and_load.py
+++ langchain_docs/vector_store/faiss/save_and_load.py
@@ -8,26 +8,37 @@ load_dotenv()
 # os.environ['FAISS_NO_AVX2'] = '1'
 
 # from langchain_openai import OpenAIEmbeddings
-from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+# from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
+# from langchain.vectorstores.faiss import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import TextLoader
 
-# loader = TextLoader("../../text_files/state_of_the_union.txt")
-# documents = loader.load()
-# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# docs = text_splitter.split_documents(documents)
-# db = FAISS.from_documents(docs, embedding)
-# db.save_local("faiss_index")
-
+loader = TextLoader("../../../text_files/state_of_the_union.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
 
 # #>>> embeddings = OpenAIEmbeddings()
-embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+# embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+# >>> saving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# db = FAISS.from_documents(docs, embedding)
+# db.save_local("faiss_index")
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# >>> loading >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+new_db = FAISS.load_local("faiss_index", embeddings=embedding, allow_dangerous_deserialization=True)
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 #>>> 保存和加载（Saving and loading）您还可以保存和加载 FAISS 索引。这很有用，因此我们不必每次使用它时都重新创建它。
 
 
 query = "What did the president say about Ketanji Brown Jackson"
-new_db = FAISS.load_local("faiss_index", embeddings=embedding, allow_dangerous_deserialization=True)
+
+
 docs = new_db.similarity_search(query)
 print(docs[0].page_content)
+# print(docs)
